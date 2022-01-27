2f5e145938cb58fe7646451c97f37760afce9d6b
JawaherAbulahsah <jawaher.abulahsah.jiz.s@tuwaiq.edu.sa>
2022-01-27 23:49:08 +0300

    work on authentication

---
+++ Jawaher_Lab1_Chat_App/Jawaher_Lab1_Chat_App/Controllers/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Jawaher_Lab1_Chat_App
+//
+//  Created by Jawaher Mohammad on 24/06/1443 AH.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
