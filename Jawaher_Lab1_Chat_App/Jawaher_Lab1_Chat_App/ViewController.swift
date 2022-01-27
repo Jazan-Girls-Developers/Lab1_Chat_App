790e051a3e6f6a7408ec2e41f4ca36f7900d3a30
JawaherAbulahsah <jawaher.abulahsah.jiz.s@tuwaiq.edu.sa>
2022-01-27 19:04:29 +0300

    update README

---
+++ Jawaher_Lab1_Chat_App/Jawaher_Lab1_Chat_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jawaher_Lab1_Chat_App
+//
+//  Created by Jawaher Mohammad on 24/06/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Lab1_Chat_App
+
+# Chat App
+
+- Real time chat application 
+
+
+# Requirement:
+
+- Email/Password Registration / Log In
+- Real Time Conversations
+- User Profile
+- Dark Mode Support
