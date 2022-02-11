15a19fe0a371b2298990479e42ade650e9b0851e
maram <maramfaqih92@gmail.com>
2022-02-12 02:32:59 +0300

    Integrate the Cloud Firestore SDKs & Initial design UI

---
+++ Lab1_Chat_App/Lab1_Chat_App/LaunchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1_Chat_App
+//
+//  Created by Maram F on 10/07/1443 AH.
+//
+
+import UIKit
+import MessageKit
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
